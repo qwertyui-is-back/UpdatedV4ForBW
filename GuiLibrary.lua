7b4fe300238d56e6cce08a6788cf959cd7cb409e
qwertyui <watchdogbestac@gmail.com>
2024-07-17 22:56:30 -0500

    Revert "Changed repo to 'Cat-V5'"
    This reverts commit 77fb64668292eff47a27bd346dc86df37b3902ce.

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -144,7 +144,7 @@ if shared.VapeExecuted then
 	local vapeCachedAssets = {}
 	local function vapeGithubRequest(scripturl)
 		if not isfile("vape/"..scripturl) then
-			local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/Cat-V5/beta/"..scripturl, true) end)
+			local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/UpdatedV4ForBW/beta/"..scripturl, true) end)
 			assert(suc, res)
 			assert(res ~= "404: Not Found", res)
 			if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
