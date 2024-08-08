28f1ad57156d6283c1ea1e6d14e4c2d9187d7c89
qwertyui <watchdogbestac@gmail.com>
2024-08-08 03:36:53 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
 shared.options = shared.options or {beta = false, sigma = false}
-local STRING = "https://raw.githubusercontent.com/qwertyui-is-back/UpdatedV4ForBW/main"
-if shared.options.beta then
-    STRING = "https://raw.githubusercontent.com/qwertyui-is-back/UpdatedV4ForBW/beta"
-end
 shared.beta = shared.options.beta
 shared.sigma = shared.options.sigma
+local STRING = "https://raw.githubusercontent.com/qwertyui-is-back/UpdatedV4ForBW/main"
+if shared.beta then
+    STRING = "https://raw.githubusercontent.com/qwertyui-is-back/UpdatedV4ForBW/beta"
+end
 return loadstring(game:HttpGet(STRING.."/NewMainScript.lua", true))()
