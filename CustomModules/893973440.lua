b75da2c756f3ff7698f0b1badb6a3b64ff3f4b00
qwertyui <watchdogbestac@gmail.com>
2024-08-09 18:21:19 -0500

    Update 893973440.lua

--- CustomModules/893973440.lua
+++ CustomModules/893973440.lua
@@ -478,7 +478,7 @@ run(function()
                     if not tweening then
                         local computer = getComputer()
                         --local slot = "ComputerTrigger"..getAvailableSlot(computer)
-                        tweenToCFrame(computer[1].CFrame, 2)
+                        tweenToCFrame(computer.ComputerTrigger3.CFrame, 2)
                     end
                 end)
             else
