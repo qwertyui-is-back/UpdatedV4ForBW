aa534f444fb3cdca3999ce2f7dc10d9566259f9f
qwertyui <watchdogbestac@gmail.com>
2024-08-11 18:26:54 -0500

    Update 893973440.lua

--- CustomModules/893973440.lua
+++ CustomModules/893973440.lua
@@ -566,7 +566,7 @@ run(function()
     local sameComp = false
     local function tweenToCFrame(cf,time,safe)
         safe = safe or false
-        if tweening then return end
+        if tweening and not store.status:lower():find("exit") then return end
         local pos = safe and 150 or 0
         lplr.Character.HumanoidRootPart.CFrame *= CFrame.new(0,pos,0)
         time = time or 0
